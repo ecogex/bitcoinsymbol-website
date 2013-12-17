41f900f4a0577bded8afa20743f5b1db9ba55d3b
ecogex <info@ecogex.com>
2013-12-17 01:13:43 +0100

    Glue

--- js/main.js
+++ js/main.js
@@ -23,6 +23,21 @@ $(function(){
     window.prompt ("Copy to clipboard: Ctrl+C, Enter", text);
   }
 
+  // Smoooth crolling
+  $(function() {
+    $('a[href*=#]:not([href=#])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top
+          }, 300);
+          return false;
+        }
+      }
+    });
+});
 
 });
 
