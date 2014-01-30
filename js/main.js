1f63e62cdc7d70d93290374eaf2b3966cdbd5d9e
ecogex <info@ecogex.com>
2014-01-30 15:28:48 +0100

    Major update - responsive template replaced with mmenu - font icon - photographs for shop - shop button

--- js/main.js
+++ js/main.js
@@ -1,66 +1,102 @@
-$(function(){
+$(function () {
 
-  
+
   var $window = $(window);
-  
+
   // Copy to clipboard
   var copied = '<p class="copied">✓ Copied</p>';
-  $('.copy').click(function(){
+  $('.copy').click(function () {
     copyToClipboard('Ƀ');
   })
-  function copyToClipboard (text) {
-    window.prompt ("Copy to clipboard: Ctrl+C, Enter", text);
+
+  function copyToClipboard(text) {
+    window.prompt("Copy to clipboard: Ctrl+C, Enter", text);
     $('.theb').parent('h1').prepend(copied);
   }
 
+  // Scroll animation
+  function scrolltop(time, href){
+    $('html, body').animate({
+      scrollTop: $(href).offset().top
+    },time);
+  }
+  
+  // Make page’s link scroll
+  var pagelinks = $('#page a[href*=#]:not([href=#])');
+  pagelinks.click(function(){
+    var href = $(this).attr('href');
+    scrolltop(200, href);
+  })
+
   // zclip (Flash)
-  function doZclip(){
+  function doZclip() {
     $('.copy').zclip({
-      path:'js/ZeroClipboard.swf',
-      copy:$('.copy').text(),
-      afterCopy:function(){
+      path: 'js/ZeroClipboard.swf',
+      copy: $('.copy').text(),
+      afterCopy: function () {
         $(this).parent('h1').prepend(copied);
       }
     })
   }
   doZclip();
 
-  // Smoooth crolling
-  $(function() {
-    $('a[href*=#]:not([href=#])').click(function() {
-      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-        if (target.length) {
-          $('html,body').animate({
-            scrollTop: target.offset().top
-          }, 300);
-          return false;
-        }
-      }
+  // Popup
+  $(document).ready(function () {
+    $('.zoom').magnificPopup({
+      type: 'image',
+      prependTo: '.popup'
     });
   });
-  
-  // Header minimizing
-  $(window).scroll( function() {
-    var value = $(this).scrollTop();
-    if ( value > $window.height()-1 ){
-      $(".min").addClass('visible');
-    }else{
-      $(".min").removeClass('visible');
-    }
-  });
-  $(window).scroll();
-    
-  // Popup
-  $(document).ready(function() {
-    $('.zoom').magnificPopup({type:'image'});
+
+  // Related to mmenu
+  var $menu = $('#menu');
+
+  // Toggle menu 
+  $('.min .theb').click(function (e) {
+    e.stopImmediatePropagation();
+    e.preventDefault();
+    $menu.trigger($menu.hasClass('mm-opened') ? 'close.mm' : 'open.mm');
   });
 
-  // Js mobile menu
-  $('nav ul').mobileMenu({switchWidth:769, topOptionText:'Menu', groupPageText:'Main', prependTo:'.mnav-sub', nested:true});
+  // Create the menu
+  $menu.mmenu({
+    position: 'right',
+    onClick: {
+      preventDefault: true,
+      setSelected: false,
+    }
+
+  });
+
+  // Click an anchor, scroll to section
+  $menu.find('a').on('click',
+    function () {
+      var href = $(this).attr('href');
+      if ($menu.hasClass('mm-opened')) {
+        $menu
+          .off('closed.mm')
+          .one('closed.mm',
+            function () {
+              setTimeout(
+                function () {
+                  scrolltop(0, href)
+                }, 10
+              );
+            }
+        );
+      } else {
+        scrolltop(200, href)
+      }
+    }
+  );
+  
+  // Cycle slideshow
+  $( '.slide' ).cycle({
+    easing:'linear',
+    timeout:3000,
+    speed:100
+    //fx:'scrollHorz',
+    //speed:8000
+  });
   
 });
-
-
-
