212120930b4726a6a46f1f6a8691daa7de882309
ecogex <info@ecogex.com>
2014-02-04 00:14:12 +0100

    Shop template folder As the shop will have its own template (header and footer). I don’t know how to move shop.php into /shop without breaking everything, that is why I only created useless and unlinked template files..

---
+++ templates/shop/_footer.php
@@ -0,0 +1,3 @@
+  <script src="<?= $base_url ?>js/shop.js"></script>
+  </body>
+</html>
---
+++ templates/shop/_header.php
@@ -0,0 +1,8 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Bitcoin Symbol Shop</title>
+    <link rel="stylesheet" href="<?= $base_url ?>css/shop.css">
+  </head>
+  <body>
