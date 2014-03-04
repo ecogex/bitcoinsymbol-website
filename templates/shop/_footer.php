d18599c9b3e585e41484a6fce9a88496ef8866ea
ecogex <info@ecogex.com>
2014-03-04 01:56:40 +0100

    Minor on footer

--- templates/shop/_footer.php
+++ templates/shop/_footer.php
@@ -1,5 +1,5 @@
+    <footer><a href="./#about">about</a></footer>
     <script src="<?= $base_url ?>js/bitcoinsymbol.js"></script>
     <script>$(initShop)</script>
   </body>
-  <footer><a href="./#about">about</a></footer>
 </html>
