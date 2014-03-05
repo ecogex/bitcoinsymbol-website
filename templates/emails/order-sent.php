eb069148126ebe4418278d4fa309dac46f01d942
ecogex <info@ecogex.com>
2014-03-05 18:59:49 +0000

    Missing templates

---
+++ templates/emails/order-sent.php
@@ -0,0 +1,9 @@
+Dear <?= $order->name ?>,
+
+Your order on Bitcoinsymbol.org has been shipped today!
+See you soon in your letterbox.
+
+Cheers from Paris.
+-- 
+Nat from Bicoinsymbol.org
+http://bitcoinsymbol.org
---
+++ templates/emails/order-trusted.php
@@ -0,0 +1,31 @@
+Hey, an order just reached 6 confirmations on Bitcoinsymbol.org.
+
+That means you can send it!
+
+Order details:
+
+<?php foreach($order->sharedProduct as $product): ?>
+# <?= $product->name ?>
+
+- Quantity: <?= $order->get_quantity($product->id) ?>
+
+- Amount: <?= $product->amount_btc() ?> BTC
+
+<?php endforeach ?>
+# Total: <?= $order->amount_btc() ?> BTC
+
+# Delivery details:
+
+Email: <?= $order->email ?>
+
+
+Name: <?= $order->name ?>
+
+
+Address:
+<?= $order->address ?>
+
+
+-- 
+Bitcoinsymbol.org
+http://bitcoinsymbol.org
