56dfc559e44e6b891411d439305b9f9138e1794f
ecogex <info@ecogex.com>
2014-03-15 15:45:03 +0100

    config -> config.example

--- config.example.php
+++ config.example.php
@@ -1,8 +1,8 @@
 <?php
 
 define('BASE_URL', 'http://localhost/bitcoinsymbol/');
-define('ADMIN_USER', 'pierre');
-define('ADMIN_PASSWORD', 'abc');
+define('ADMIN_USER', 'admin');
+define('ADMIN_PASSWORD', 'password');
 
 define('DEBUG', TRUE);
 
@@ -19,3 +19,4 @@ define('DATA_DIR', __DIR__ . '/data');
 define('CONTROLLERS_DIR', __DIR__ . '/controllers');
 define('TEMPLATES_DIR', __DIR__ . '/templates');
 define('CALLBACK_HASH_SEED', '>94DY)(-fA&`m,*');
+
