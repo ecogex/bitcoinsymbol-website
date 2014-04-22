01feb881cd9fb613e1df9640123ec1b9809a70e4
ecogex <info@ecogex.com>
2014-04-22 18:04:07 +0200

    + article

--- templates/home.php
+++ templates/home.php
@@ -354,6 +354,7 @@
             <li><a href="http://www.cryptocoinsnews.com/news/bitcoin-com-gets-revamp-blockchain-info/2014/04/17">Cryptocoins News</a></li>
             <li><a href="http://www.bitcoinx.com/french-design-company-ecogex-proposes-unicode-%C9%83-new-bitcoin-symbol/">Bitcoinx</a></li>
             <li><a href="http://bitcoinmagazine.com/product/issue03/">Bitcoin Magazine #3</a></li>
+            <li><a href="http://bangkok.coconuts.co/2014/04/22/bh-or-not-b-bitcoin-movement-debates-stealing-thai-bahts-identity">Coconuts Bangkok</a></li>
             <li><a href="http://www.dailydot.com/business/bitcoin-logo-new-unicode/">Dailydot</a></li>
             <li><a href="http://www.dcmagnates.com/new-bitcoin-symbol-suggested-by-industry-players/">DC Magnates</a></li>
             <li><a href="http://www.ihavebitcoins.com/featured/bitcoin-game-name-dropping-symbol-slipping/">I have bitcoins</a></li>
